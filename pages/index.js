4a7279310b57b2e18f5be6706603701e4cd15ad6
rafaelpignataro <rafael@mswebmaker.com>
2021-01-31 16:05:06 -0300

    Fixed GitHubCorner button link

--- pages/index.js
+++ pages/index.js
@@ -108,7 +108,7 @@ export default function Home() {
           animate="show"
         />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/rafapignataro" />
+      <GitHubCorner projectUrl="https://github.com/rafapignataro/quiz_nextjs" />
     </QuizBackground>
   );
 }
